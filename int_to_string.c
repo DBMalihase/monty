521749e5058b42c8885521154b1bdb88856f3d24
DBMalihase <dennismalihase@gmail.com>
2022-12-15 13:11:11 -0800

    Stack, Queues - LIFO, FIFO

---
+++ int_to_string.c
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+
+char *get_int(int num);
+unsigned int _abs(int);
+int get_numbase_len(unsigned int num, unsigned int base);
+void fill_numbase_buff(unsigned int num, unsigned int base,
+		char *buff, int buff_size);
+
+/**
+ * get_int - gets a char pointer to new string containing int
+ * @num: number to convert to string
+ * Return: char pointer to newly created string otherwise NULL
+ */
+
+char *get_int(int num)
+{
+	unsigned int temp;
+	int length = 0;
+	long num_l = 0;
+	char *ret;
+
+	temp = _abs(num);
+	length = get_numbase_len(temp, 10);
+
+	if (num < 0 || num_l < 0)
+		length++;
+	ret = malloc(length + 1);
+	if (!ret)
+		return (NULL);
+
+	fill_numbase_buff(temp, 10, ret, length);
+	if (num < 0 || num_l < 0)
+		ret[0] = '-';
+
+	return (ret);
+}
+
+/**
+ * _abs - gets the value of an int
+ * @i: int to get value of
+ * Return: unsigned int abs rep of i
+ */
+
+unsigned int _abs(int i)
+{
+	if (i < 0)
+		return (-(unsigned int)i);
+	return ((unsigned int)i);
+}
+
+/**
+ * get_numbase_len - gets length of buffer needed for an unsigned int
+ * @num: num to get length needed for
+ * @base: base of number representation used by buffer
+ * Return: int containing length of buffer needed
+ */
+
+int get_numbase_len(unsigned int num, unsigned int base)
+{
+	int len = 1;
+
+	while (num > base - 1)
+	{
+		len++;
+		num /= base;
+	}
+	return (len);
+}
+
+/**
+ * fill_numbase_buff - fills buffer with correct num up to base 36
+ * @num: num to convert to string given base
+ * @base: base of num used in conversion
+ * @buff: buffer to fill with result of conversion
+ * @buff_size: size of buffer in bytes
+ * Return: void
+ */
+
+void fill_numbase_buff(unsigned int num, unsigned int base,
+		char *buff, int buff_size)
+{
+	int rem, i = buff_size - 1;
+
+	buff[buff_size] = '\0';
+	while (i >= 0)
+	{
+		rem = num % base;
+		if (rem > 9)
+			buff[i] = rem + 87;
+		else
+			buff[i] = rem + '0';
+		num /= base;
+		i--;
+	}
+}
